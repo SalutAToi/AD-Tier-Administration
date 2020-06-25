3897b7b022e6933105bf4a1a80a274339ce67aae
SalutAToi <5493696+SalutAToi@users.noreply.github.com>
2020-06-25 12:49:52 +0200

    improving group creation script

--- Create-Groups.ps1
+++ Create-Groups.ps1
@@ -4,21 +4,35 @@ $PathHeader = "Path"
 $GroupCategoryHeader = "GroupCategory"
 $GroupScopeHeader = "GroupScope"
 
+function Test-Group ($GroupName) {
+    
+    $ErrorActionPreference = "silentlycontinue"
+    $Group = Get-ADGroup -Identity $GroupName
+
+    if ($Group -eq $null) {
+        return $false
+    }
+    else {
+        return $true
+    }
+}
+
 
 # iterate through each entry of the csv
 Import-Csv $CSVPath | ForEach-Object {
 
     # check if group already exists and skips that loop if it does
-    if (Get-ADGroup -Identity $($_.$NameHeader) -ErrorAction SilentlyContinue) {
+    if (Test-Group $_.$NameHeader) {
+        Write-Host "$($_.$NameHeader) exists, skipping."
         return
     }
 
     # prompt action and create group
-    Write-Host "Creating -Name $($_.$NameHeader) -Path $($_.$PathHeader) -GroupCategory $($_.$GroupCategoryHeader) -GroupScope $($_.$GroupScopeHeader)"
+    Write-Host "Creating $($_.$NameHeader) at $($_.$PathHeader), category $($_.$GroupCategoryHeader), scope $($_.$GroupScopeHeader)"
     New-ADGroup -Name $($_.$NameHeader) -Path $($_.$PathHeader) -GroupCategory $($_.$GroupCategoryHeader) -GroupScope $($_.$GroupScopeHeader)
 
     # check if existant, prompt if not
-    if (! (Get-ADGroup -Identity $($_.$NameHeader) -ErrorAction SilentlyContinue)) {
-        Write-Host -ForegroundColor Red -BackgroundColor Black "ERROR : Group "$($_.$NameHeader)" was not created"
+    if (! (Test-Group $_.$NameHeader)) {
+        Write-Host -ForegroundColor Red -BackgroundColor Black "ERROR : Group $($_.$NameHeader) was not created"
     }
 }
