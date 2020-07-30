304c9d19d8d49fa5ca857634f327dfa7c7ebd6e1
SalutAToi <5493696+SalutAToi@users.noreply.github.com>
2020-07-30 06:49:27 +0200

    adding Tier users to relevant Tier group when creating

--- Create-Users.ps1
+++ Create-Users.ps1
@@ -10,19 +10,22 @@ $CSVObject | ForEach-Object {
 
     if ($_.$Tier0Header -eq $true) {
         $Username = "AT0_" + $_.$UsernameHeader
-        New-ADUser -Path "OU=Users,OU=Tier 0,OU=Administration,OU=Corp,DC=contoso,DC=com" -Name $Username -Enabled $false
+        New-ADUser -Path "OU=Users,OU=Tier 0,OU=Administration,OU=Corp,DC=CATERCARE,DC=LOCAL" -Name $Username -Enabled $false
         Write-Host "Created user $Username"
+        Add-ADGroupMember -Identity 'ROLE_Tier 0 Admin' -Members $Username
     }
 
     if ($_.$Tier1Header -eq $true) {
         $Username = "AT1_" + $_.$UsernameHeader
-        New-ADUser -Path "OU=Users,OU=Tier 1,OU=Administration,OU=Corp,DC=contoso,DC=com" -Name $Username -Enabled $false
+        New-ADUser -Path "OU=Users,OU=Tier 1,OU=Administration,OU=Corp,DC=CATERCARE,DC=LOCAL" -Name $Username -Enabled $false
         Write-Host "Created user $Username"
+        Add-ADGroupMember -Identity 'ROLE_Tier 1 Admin' -Members $Username
     }
 
     if ($_.$Tier2Header -eq $true) {
         $Username = "AT2_" + $_.$UsernameHeader
-        New-ADUser -Path "OU=Users,OU=Tier 2,OU=Administration,OU=Corp,DC=contoso,DC=com" -Name $Username -Enabled $false
+        New-ADUser -Path "OU=Users,OU=Tier 2,OU=Administration,OU=Corp,DC=CATERCARE,DC=LOCAL" -Name $Username -Enabled $false
         Write-Host "Created user $Username"
+        Add-ADGroupMember -Identity 'ROLE_Tier 2 Admin' -Members $Username  
     }
 }
